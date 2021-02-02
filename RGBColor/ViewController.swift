d4e02cee0f51840d344eb55a9d35a4e2d525d7ba
ihValery <ihValery@gmail.com>
2021-02-03 01:15:30 +0300

    add rgbSliderAction добавили к слайду tag  и по ним в switch привязали значения к Label u TextField

--- RGBColor/ViewController.swift
+++ RGBColor/ViewController.swift
@@ -9,11 +9,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redTextField: UITextField!
+    @IBOutlet weak var greenTextField: UITextField!
+    @IBOutlet weak var blueTextField: UITextField!
+    
     override func viewDidLoad() {
+        
+        setMyDesign()
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func rgbSliderAction(_ sender: UISlider) {
+        switch sender.tag {                                                //выделяем Slider -> Attributes inspector -> View -> Tag
+        case 0:
+            redLabel.text = String(sender.value)
+            redTextField.text = String(sender.value)
+        case 1:
+            greenLabel.text = String(sender.value)
+            greenTextField.text = String(sender.value)
+        case 2:
+            blueLabel.text = String(sender.value)
+            blueTextField.text = String(sender.value)
+        default:
+            break
+        }
+        
+        
+    }
+    
+    
+    func setMyDesign() {
+        colorView.layer.cornerRadius = 15                                   //закругление краев
+        
+        redSlider.tintColor = .red
+        greenSlider.tintColor = .green
+//        blueSlider.tintColor = .systemBlue
+    }
+    
+    
+//    func setColor() {                                                       //установка цвета
+//        <#function body#>
+//    }
+//
+//    func setValueForLabel() {                                               //установка значения для лейбла
+//        <#function body#>
+//    }
+//
+//    func setValueForTextField() {                                           //установка значения для текстового поля
+//        <#function body#>
+//    }
+    
 }
 
